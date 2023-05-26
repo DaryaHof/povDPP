6474569580597882c415a80a1735582ecb305fd7
DaryaHof <darya.hofrichter@gmail.com>
2023-05-26 16:07:53 +0200

    26.5

--- chovani_ridice_slova.py
+++ chovani_ridice_slova.py
@@ -1,9 +1,11 @@
+# Importujeme knihovnu Pandas
 import pandas as pd
 
-# Soubor .csv s jednim sloupcem, ktery obsahuje podnety s tematem "Chovani ridice"
+# Ukladame si tabulku se sloupcem obsahujicim texty podnetu s tematem "Chovani ridice" 
+# do promenne
 podnety = pd.read_csv('POV_POPIS_CHOVANI_RIDICE.csv', encoding='utf-8')
 
-# Prevadime na seznam
+# Prevadime promennou na seznam
 slova = podnety['POV_POPIS'].tolist()
 
 # Odstranujeme nepotrebne znaky
@@ -33,20 +35,14 @@ slova = ' '.join(str(slovo) for slovo in slova)
 # Rozdelujeme dokument na slova
 nova_slova = slova.split(' ')
 
-# # muzeme i vynechat prazdna slova rovnou tady
-# words = [w for w in words if w ]
-
 # Pripravujeme data pro DataFrame
 data = {'Slovo': nova_slova}
 
 # Vytvarime DataFrame
 df = pd.DataFrame(data)
 
-# Vytvorime sloupec ID s automaticky generovanými ID
+# Vytvarime sloupec ID s automaticky generovanými ID
 df.insert(0, 'ID', range(1, 1 + len(df)))
 
-# Zobrazime vyslednou tabulku
-print(df)
-
-# Ulozime tabulku do souboru csv
+# Ukladame si tabulku do souboru csv
 df.to_csv('slova_chovani_ridice.csv', index=False)
---
+++ korelace_delka_vyrizeni_delka_podnetu.py
@@ -0,0 +1,26 @@
+# Importujeme knihovnu Pandas
+import pandas as pd
+
+# Importujeme knihovnu Seaborn
+import seaborn as sns
+
+# Importujeme knihovnu Matplotlib.pyplot
+import matplotlib.pyplot as plt
+
+# Ukladame si tabulku do promenne
+data = pd.read_csv('pov_podnety.csv', encoding='utf-8')
+
+# Vybirame z tabulky pouze ty sloupce, ktere nas zajimaji
+tabulka = data[['DELKA_VYRIZENI_PODNETU', 'POV_POPIS']]
+
+# Pridavame do tabulky sloupec s vypoctem delky textu podnetu
+tabulka.loc[:, 'DELKA_PODNETU'] = data['POV_POPIS'].str.len()
+
+# Vyfiltrovavame si pouze ty podnety, kde delka vyrizeni stiznosti je mensi,
+# nez 1 rok (ostatni podnety obsahuji chyby)
+nova_tabulka = tabulka[tabulka['DELKA_VYRIZENI_PODNETU'] < 365]
+
+# Zobrazujeme korelacni graf
+g = sns.JointGrid(data=nova_tabulka, x='DELKA_VYRIZENI_PODNETU', y='DELKA_PODNETU')
+g.plot_joint(sns.scatterplot, color='red', legend=False)
+plt.show()
